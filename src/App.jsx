47442f806f14f599060ecb381bf2dfcf8ba5ca5c
Salamat <temirbolotov3@gmail.com>
2026-02-17 15:30:30 +0600

    app js ти ондодум

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-<<<<<<< HEAD
+
 import Catalog from './components/Catalog'
-=======
 import Home from './components/Home'
->>>>>>> 5839e2222e6ab1c50971b65dcca7ea6e800c0a79
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<<<<<<< HEAD
-      <Catalog/>
-=======
-      <Home/>
->>>>>>> 5839e2222e6ab1c50971b65dcca7ea6e800c0a79
+      <Home />
+      <Catalog />
+
     </>
   )
 }
